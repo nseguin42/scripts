57bee19bac634b84e7f798f88ccc3be50254a802
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2022-05-15 08:04:35 -0500

    add fzkill.sh

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # scripts
 A collection of scripts to do various things.
+
+fzkill.sh: fuzzy search (via `fuzzel`) through process list (via `ps`) and send a kill signal (optional argument, default 15)
---
+++ fzkill.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+re='^[0-9]+$'
+signal=15 #SIGTERM
+if [[ $1 =~ $re ]] ; then
+  signal=$1
+elif [ $1 ]; then
+  echo "Not a number: $1" 
+  echo "Pass a number as the first argument to use it as a kill signal. Defaults to 15 (SIGTERM)."
+  exit
+fi
+
+ps="$(ps -U $USER -u $USER -eo comm,pid,pcpu,args  --sort -pcpu  --no-headers)"
+
+# in: a newline-separated list
+# out: the selected line, e.g.
+# `fzkill           334411  0.0 /bin/bash /home/user/.local/bin/fzkill`
+dmenu_cmd='fuzzel -I -w 120 -l 10 -b 3b4252ef -t d8dee9ff -s 88c0d0ff -S 3b4252ff -C 4c566aef -m bf616aff -r 0 --line-height=24px -d'
+
+process="$(echo "$ps" | eval "$dmenu_cmd")"
+process_=($process)
+pid=${process_[1]}
+
+# quiet exit if pid is empty, loud if PID is invalid
+if [ -z "${pid}" ]; then
+  exit
+elif ! $(kill -s 0 "$pid" &>/dev/null); then
+  echo "Error: process with PID '$pid' does not exist."
+  exit
+fi
+
+exec kill -"$signal" "$pid" 
